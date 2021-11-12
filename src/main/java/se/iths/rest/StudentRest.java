c8322e9a3343cad98d7db6395b84a0543bfcd158
biniam haile <biniamhk@gmail.com>
2021-11-12 15:30:46 +0100

    create student method added

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,34 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
+
+    @Inject
+    StudentService studentService;
+
+    @Path("")
+    @POST
+    public Response createStudent(Student student){
+        if(student.getFirstName()==null || student.getLastName()==null ||
+                student.getEmail()==null)
+            throw new WebApplicationException(Response.status(Response.Status.NO_CONTENT)
+                    .entity("Some data is missing").type(MediaType.TEXT_PLAIN_TYPE).build());
+
+
+            studentService.createStudent(student);
+
+        return Response.ok(student).build();
+    }
 
 
 
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,19 @@
 package se.iths.service;
 
 
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
 public class StudentService {
+    @PersistenceContext
+    EntityManager entityManager;
+    public void createStudent(Student student){
+        entityManager.persist(student);
+    }
 
 
 }
